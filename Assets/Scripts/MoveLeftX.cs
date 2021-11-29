6fa09f1de9aaa7652d4224a9784fdb47deaf67d1
Garrett Moore <28462557+garrettmmoore@users.noreply.github.com>
2021-11-29 01:48:22 -0800

    Fix to allow Background and Obstacles to move left when the game is not over

--- Assets/Scripts/MoveLeftX.cs
+++ Assets/Scripts/MoveLeftX.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 public class MoveLeftX : MonoBehaviour
 {
     public float speed;
-    private PlayerControllerX playerControllerScript;
-    private float leftBound = -10;
+    private PlayerControllerX _playerControllerScript;
+    private const float LeftBound = -10;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerControllerX>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         // If game is not over, move to the left
-        if (playerControllerScript.gameOver)
+        if (_playerControllerScript.gameOver == false)
         {
             transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
         }
 
         // If object goes off screen that is NOT the background, destroy it
-        if (transform.position.x < leftBound && !gameObject.CompareTag("Background"))
+        if (transform.position.x < LeftBound && !gameObject.CompareTag("Background"))
         {
             Destroy(gameObject);
         }
